ab63fc3211c2b389d33a308e13f8042be0f36dba
邓强杰 <83818053+deng-7@users.noreply.github.com>
2021-09-03 23:41:42 +0800

    github修改

--- src/main/java/com/xxx/git/GitTest.java
+++ src/main/java/com/xxx/git/GitTest.java
@@ -16,5 +16,6 @@ public class GitTest {
         System.out.println("push test");
         System.out.println("push test2");
         System.out.println("Hello Git6");
+        System.out.println("Hello Git7");
     }
 }
