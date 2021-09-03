a978f77c0fff0e05a636f482f9e00d8caa94cc8f
deng-7 <1522764046@qq.com>
2021-09-03 22:14:53 +0800

    第一次提交

---
+++ src/main/java/com/xxx/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.xxx.git;
+
+/**
+ * @author DQJ
+ * @description: Git测试
+ * @date 2021/9/3 22:09
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
