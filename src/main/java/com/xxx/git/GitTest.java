fe4729a76e666022f29304e00fcd09ad8a1d0c6b
deng-7 <1522764046@qq.com>
2021-09-03 22:18:14 +0800

    修改GitTest

--- src/main/java/com/xxx/git/GitTest.java
+++ src/main/java/com/xxx/git/GitTest.java
@@ -8,5 +8,6 @@ package com.xxx.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("Hello Git2");
     }
 }
